cfba0e6cf4925426fd71bfc5bffea4f6617afaeb
davidyakoub <david.yakoub2008@gmail.com>
2026-05-08 23:10:47 -0700

    changed node name, made player class, made rat scene

--- cisg-110-final/scenes/rat.gd
+++ cisg-110-final/scenes/rat.gd
@@ -1,8 +1,18 @@
 extends RigidBody2D
 
+@export var _explosion_VFX: CPUParticles2D
+
 @export var _speed: float = 50.0
 
 func _physics_process(delta: float) -> void:
 	linear_velocity.x = -_speed
 	
 	move_and_collide(linear_velocity * delta)
+
+
+func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
+	if body is Player:
+		_explosion_VFX.emitting = true
+		
+		_explosion_VFX.reparent(get_parent())
+		queue_free()
